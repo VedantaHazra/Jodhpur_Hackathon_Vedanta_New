a841de4ae8a08718bef8c291b0ea43d13bdfe9c9
ajaytiwari0210 <119790882+VedantaHazra@users.noreply.github.com>
2024-01-20 19:24:03 +0530

    Gun added

--- Assets/Scripts/ReturnScript.cs
+++ Assets/Scripts/ReturnScript.cs
@@ -6,8 +6,16 @@ public class ReturnScript : MonoBehaviour
 {
     public Transform playerTransform;
     public Vector3 pos = new Vector3(-86f, -2.89f, -43f);
+
+    public GameObject gun;
+    public Transform spawnPoint;
     public void Return()
     {
         playerTransform.position = pos;
+    }
+
+    public void InstantiateGun()
+    {
+        Instantiate(gun, spawnPoint.position, Quaternion.identity);
     }
 }
